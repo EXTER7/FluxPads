d86b2deda1a692b5bc5d0f7e3cdb5e40394dc085
EXTER <exter7@gmail.com>
2015-08-25 23:42:37 -0400

    Tweaked RF storage.

--- src/main/java/exter/fluxpads/config/FluxPadsConfig.java
+++ src/main/java/exter/fluxpads/config/FluxPadsConfig.java
@@ -19,16 +19,16 @@ public class FluxPadsConfig
 
   static public void load(Configuration config)
   {
-    basic_capacity = config.get("fluxpads", "fluxpad.basic.capacity", 100000).getInt(100000);    
+    basic_capacity = config.get("fluxpads", "fluxpad.basic.capacity", 10000).getInt(10000);    
     basic_rate = config.get("fluxpads", "fluxpad.basic.rate", 100).getInt(100); 
 
-    hardened_capacity = config.get("fluxpads", "fluxpad.hardened.capacity", 500000).getInt(500000);    
+    hardened_capacity = config.get("fluxpads", "fluxpad.hardened.capacity", 50000).getInt(50000);    
     hardened_rate = config.get("fluxpads", "fluxpad.hardened.rate", 400).getInt(400);
 
-    reinforced_capacity = config.get("fluxpads", "fluxpad.reinforced.capacity", 5000000).getInt(5000000);    
+    reinforced_capacity = config.get("fluxpads", "fluxpad.reinforced.capacity", 500000).getInt(500000);    
     reinforced_rate = config.get("fluxpads", "fluxpad.reinforced.rate", 2000).getInt(2000);
 
-    resonant_capacity = config.get("fluxpads", "fluxpad.resonant.capacity", 20000000).getInt(20000000);    
+    resonant_capacity = config.get("fluxpads", "fluxpad.resonant.capacity", 2000000).getInt(2000000);    
     resonant_rate = config.get("fluxpads", "fluxpad.resonant.rate", 8000).getInt(8000);
   }
 }
